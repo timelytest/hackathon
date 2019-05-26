bcc91aad7f128108949d8e8894aefe0c83379071
wangnig <1126918543@qq.com>
2019-05-26 14:34:29 +0800

    新增图片

--- android/cainiaoxiaozu/app/src/main/java/com/example/cainiaoxiaozu/ui/fragment/HomeFragment.java
+++ android/cainiaoxiaozu/app/src/main/java/com/example/cainiaoxiaozu/ui/fragment/HomeFragment.java
@@ -7,14 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ScrollView;
 
 import com.example.cainiaoxiaozu.R;
 
+import cn.bingoogolapple.bgabanner.BGABanner;
+
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "fragment_home";
-    private ScrollView scrollView;
+    private FrameLayout frameLayout;
+    private BGABanner bgaBanner;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,8 +29,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        scrollView = view.findViewById(R.id.fragment_home);
-
+        frameLayout = view.findViewById(R.id.fragment_home);
+        bgaBanner = view.findViewById(R.id.banner_guide_content);
         return view;
     }
 }
